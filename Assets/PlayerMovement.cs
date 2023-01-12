faf9fd7fbe88d8a50aa9e59c163d19c222044b1b
HugoIhlman <hugo.ihlman@elev.ga.lbs.se>
2023-01-12 15:05:25 +0100

    mov

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    int playerspeed = 10;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * playerspeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= Vector3.up * playerspeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * playerspeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Vector3.right * playerspeed * Time.deltaTime;
+        }
     }
 }
