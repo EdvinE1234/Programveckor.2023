6cc2e487a64fa87bfe80a0eebbdbe79ad599c2bb
williamahlen <120007845+williamahlen@users.noreply.github.com>
2023-01-16 10:43:11 +0100

    7648

--- Assets/Scenes/pathpart2.cs
+++ Assets/Scenes/pathpart2.cs
@@ -48,17 +48,6 @@ public class pathpart2 : MonoBehaviour
             waypointindex = 0;
         }
     }
-    private void FixedUpdate()
-    {
-        if (player != null)
-        {
-            Vector3 vectorToTarget = player.transform.position - transform.position;
-            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationModifier;
-            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
-        }
-
-    }
 
 }
 
---
+++ Assets/Scenes/rotation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public float rotatespeed;
+    public bool autorotate;
+    // Update is called once per frame
+    void Update()
+    {
+        if (autorotate)
+        {
+            transform.Rotate(0, 0 , rotatespeed * Time.deltaTime);
+        }
+    }
+}
